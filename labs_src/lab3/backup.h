af9ea60d5035508b0328a39f7bce64338ac9140b
EgorChernov5 <EgorChernov3140@gmail.com>
2022-04-16 14:54:02 +0300

    Made main files

---
+++ labs_src/lab3/backup.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include "elzip.hpp"
+
+class Backup {
+
+};
+
+class RestorePoint {
+
+};
+
+class BackupJob {
+
+};
+
+class JobObject {
+
+};
+
+class Storage {
+
+};
+
+class Repository {
+
+};
