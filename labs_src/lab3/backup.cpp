fa210546cd87b72e8ff4cdacd58d9d41f0607ecc
EgorChernov5 <EgorChernov3140@gmail.com>
2022-04-19 18:16:50 +0300

    step 1

--- labs_src/lab3/backup.cpp
+++ labs_src/lab3/backup.cpp
@@ -1,1 +1,71 @@
 #include "backup.h"
+
+void BackupJob::setName(std::string name) {
+    this->name = name;
+}
+
+void BackupJob::setZipType(std::string type) {
+    this->type = type;
+    if (type == "Split storages") {
+        this->type = type;
+    } else if (type == "Single storage") {
+        this->type = type;
+    } else {
+        throw std::invalid_argument("Type didn't found.");
+    }
+}
+
+void BackupJob::setPath(std::string path) {
+    this->path = path;
+}
+
+void BackupJob::addObject(JobObject *file) {
+    this->files.push_back(file->getName());
+}
+
+void BackupJob::removeObject(JobObject *file) {
+    auto n = find(this->files.begin(), this->files.end(), file->getName());
+    this->files.erase(n);
+}
+
+void BackupJob::createRestorePoint() {
+
+}
+
+JobObject::JobObject(std::string path) {
+    this->path = path;
+    this->name = path.substr(path.find_last_of('/') + 1);
+}
+
+std::string JobObject::getName() {
+    return name;
+}
+
+void Storage::copy(std::vector<std::string> files, std::string type, std::string path) {
+    std::ofstream fout;
+    std::ifstream fin;
+    if (type == "Split storages") {
+        for (int i = 0; i < files.size(); i++) {
+            try {
+                std::string str;
+                fin.open(files[i]);
+                fout.open(path);
+                while (!fin.eof()) {
+                    str = "";
+                    getline(fin, str);
+                    fout << str;
+                }
+            }
+            catch (const std::exception &ex) {
+                std::cout << ex.what() << std::endl;
+                throw std::invalid_argument("Path isn't correct.");
+            }
+
+
+        }
+    } else if (type == "Single storage") {
+
+    } else {
+
+    }
+}
--- labs_src/lab3/backup.h
+++ labs_src/lab3/backup.h
@@ -1,7 +1,12 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include <string>
 #include <fstream>
 #include "elzip.hpp"
+#include <algorithm>
+
+class JobObject;
 
 class Backup {
 
@@ -12,17 +17,36 @@ class RestorePoint {
 };
 
 class BackupJob {
-
+private:
+    std::string name;
+    std::string type;
+    std::string path;
+    std::vector<std::string> files;
+public:
+    void setName(std::string name);
+    void setZipType(std::string type);
+    void setPath(std::string path);
+    void addObject(JobObject *file);
+    void removeObject(JobObject *file);
+    void createRestorePoint();
 };
 
 class JobObject {
-
+private:
+    std::string path;
+    std::string name;
+public:
+    JobObject(std::string path);
+    std::string getName();
 };
 
 class Storage {
-
+public:
+    void copy(std::vector<std::string> files, std::string type, std::string path);
 };
 
 class Repository {
-
+    void zipDir();
+    void unzipDir();
+    void unzipFile();
 };
